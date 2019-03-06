1fff2c18fe823f7ee30f6610fc73c94229fc5cc7
AnDrIYQ <48154901+AnDrIYQ@users.noreply.github.com>
2019-03-06 14:40:33 +0200

    Added a Test Header
    Added a Test Header

--- public_html/application/controllers/Main.php
+++ public_html/application/controllers/Main.php
@@ -4,10 +4,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Main extends CI_Controller {
 
 	public function index()
+
 	{
-
-		 $this->load->view('jqe');
-
+		$this->load->view('page/header');
+		$this->load->view('page/aside');
+		$this->load->view('page/article');
+		$this->load->view('page/footer');
 	}
 
 
